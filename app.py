03d04bd86ab61cd3d842257a0cdf0e75b035ffac
NavyAF1 <97703721+NavyAF1@users.noreply.github.com>
2023-02-21 21:33:13 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -2,9 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+SERVICES = [{
+  'id': 1,
+  'title': 'Ayurveda Consultation - IN-PERSON',
+  'location': 'In-person',
+  'price': '$120/hour'
+}, {
+  'id': 2,
+  'title': 'FOLLOW-UP:Ayurveda Consultation IN-PERSON',
+  'location': 'In-person',
+  'price': '3 months: $75, 3-6 months: $90'
+}, {
+  'id': 3,
+  'title': 'Ayurveda Consultation - ONLINE',
+  'location': 'VIRTUAL ONLINE',
+  'price': '$120/hour'
+}, {
+  'id': 4,
+  'title': 'FOLLOW-UP:Ayurveda Consultation ONLINE',
+  'location': 'VIRTUAL ONLINE',
+  'price': '$75/30 minutes'
+}]
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', services=SERVICES)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
